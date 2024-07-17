0989f3ef9a5223a734252ec235e64694297dda74
rizkybp31 <rizkybayuprasetyo@gmail.com>
2024-07-17 23:27:42 +0700

    edit style in About.jsx

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <Container className="my-5">
       <h1 className="fw-bold text-center my-5">Tentang Website BanjirPedia</h1>
-      <p className="fs-6 text-dark text-center mx-auto my-5 col-9">
+      <p className="fs-6 text-dark text-center mx-auto my-5 col-lg-9">
         Banjirpedia adalah portal informasi yang didedikasikan untuk memberikan
         edukasi dan informasi penting terkait banjir kepada warga Kelurahan
         Cabean, Semarang Barat, Semarang. Kami bertujuan untuk meningkatkan
